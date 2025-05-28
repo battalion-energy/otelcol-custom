89166a60286f3529e1d031684178e37161212fbf
jadamcrain <jadamcrain@gmail.com>
2025-05-28 11:40:32 -0700

    fix typo in exec-start-pre script

--- otelcol-exec-start-pre.sh
+++ otelcol-exec-start-pre.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-for v in MOUNT_POINTS NETWORK_INTERFACES DASH0_KEY ; do
+for v in MOUNT_POINTS NETWORK_INTERFACES DASH0_API_KEY ; do
     [ -n "${!v}" ] || { echo "$v is missing"; exit 1; }
 done
