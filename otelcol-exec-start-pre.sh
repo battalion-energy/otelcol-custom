d746d6b548d4e5ab637d2e36a550e8ae53313a97
jadamcrain <jadamcrain@gmail.com>
2025-05-28 12:05:45 -0700

    parameterize metrics scrape and batch intervals via env vars

--- otelcol-exec-start-pre.sh
+++ otelcol-exec-start-pre.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-for v in MOUNT_POINTS NETWORK_INTERFACES DASH0_API_KEY ; do
+for v in MOUNT_POINTS NETWORK_INTERFACES DASH0_API_KEY METRICS_SCRAPE_INTERVAL METRICS_BATCH_INTERVAL; do
     [ -n "${!v}" ] || { echo "$v is missing"; exit 1; }
 done
